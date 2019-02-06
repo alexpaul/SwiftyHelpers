c6be98b782df372f336cfea165621d38dd0de20c
Alex Paul <alexpaul@users.noreply.github.com>
2019-02-05 20:41:28 -0500

    Update Date+Extensions.swift

--- Date+Extensions.swift
+++ Date+Extensions.swift
@@ -1,3 +1,11 @@
+//
+//  Date+Extensions.swift
+//
+//  Created by Alex Paul on 1/30/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+import Foundation
+
 extension Date {
   // get an ISO timestamp
   static func getISOTimestamp() -> String {
