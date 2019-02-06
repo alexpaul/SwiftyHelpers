acfc378ed0fcdecc9a522161b8f5b1ff1d059af3
Alex Paul <alexpaul@users.noreply.github.com>
2019-02-05 20:46:59 -0500

    Create ImageCache.swift

---
+++ ImageCache.swift
@@ -0,0 +1,35 @@
+//
+//  ImageCache.swift
+//
+//  Created by Alex Paul on 1/23/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+final class ImageCache {
+  private init() {}
+  
+  private static var cache = NSCache<NSString, UIImage>()
+  
+  static func fetchImageFromNetwork(urlString: String, completion: @escaping (AppError?, UIImage?) -> Void) {
+    NetworkHelper.shared.performDataTask(endpointURLString: urlString) { (appError, data ) in
+      if let appError = appError {
+        completion(appError, nil)
+      } else if let data = data {
+        DispatchQueue.global().async {
+          if let image = UIImage(data: data) {
+            cache.setObject(image, forKey: urlString as NSString)
+            DispatchQueue.main.async {
+              completion(nil, image)
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  static func fetchImageFromCache(urlString: String) -> UIImage? {
+    return cache.object(forKey: urlString as NSString)
+  }
+}
